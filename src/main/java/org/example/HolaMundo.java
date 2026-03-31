cb0cc8b5a580cb113122ad4bcdab8f1a5f7a25ea
jonam <j.manquel02@ufromail.cl>
2026-03-31 02:12:19 -0300

    agrega nombre y apellido

--- src/main/java/org/example/HolaMundo.java
+++ src/main/java/org/example/HolaMundo.java
@@ -3,5 +3,6 @@ package org.example;
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!!!!");
+        System.out.println("Jonathan Eduardo Manquel Aniñir");
     }
 }
